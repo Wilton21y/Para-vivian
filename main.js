5c41ca301e6fb2cf649b675a0e026b16c6d68a7a
Wilton de Oliveira <104309027+Wilton21y@users.noreply.github.com>
2025-11-16 21:25:55 -0300

    Add files via upload
    Rott arquivos

---
+++ main.js
@@ -0,0 +1,15 @@
+ var img = document.getElementById('photo')
+
+function mostraImg() {
+ img.style. visibility='hidden'
+
+
+}
+
+const bt2 = document.getElementById('bt2');
+
+function ocutaImg() {
+ // Tab to edit
+ img.style.visibility= "visible"
+
+}
